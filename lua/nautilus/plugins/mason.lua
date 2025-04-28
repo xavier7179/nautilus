e6110848a6a0c9f699f526ccc5e927133ceb3de5
Ale <notinmyhead@gmail.com>
2025-04-28 18:33:02 +0200

    Solved the Mason plugin chain issue
    - Solved a bug that generate an error in the Mason instatiantion.
    - After better reading the mason-tool-installer documentation, cleaned up the mason.lua file as now that package handles all installations (lsp, linter & formatters and dap)

--- lua/nautilus/plugins/mason.lua
+++ lua/nautilus/plugins/mason.lua
@@ -1,85 +1,58 @@
 return {
     {
         "jay-babu/mason-nvim-dap.nvim",
-        --       event = "VeryLazy",
-        -- cmd = { "DapInstall", "DapUninstall" },
-        -- dependencies = {
-        --    "williamboman/mason.nvim",
-        --    "mfussenegger/nvim-dap",
-        -- },
+        cmd = { "DapInstall", "DapUninstall" },
+        dependencies = {
+            "williamboman/mason.nvim",
+            --    "mfussenegger/nvim-dap",
+        },
     },
     "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
     {
         "williamboman/mason.nvim",
-        config = function()
+        cmd = "Mason",
+        keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+        build = ":MasonUpdate",
+        opts = {
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
+            },
+        },
+        config = function(_, opts)
             -- import mason
             local mason = require("mason")
-
-            -- import mason-lspconfig
-            local mason_lspconfig = require("mason-lspconfig")
-            local mason_nvim_dap = require("mason-nvim-dap")
-            local mason_tool_installer = require("mason-tool-installer")
-            mason.setup()
-            mason_lspconfig.setup({
-                automatic_installation = true,
-                ensure_installed = {
-                    "clangd", -- C/C++
-                    --					"neocmake",
-                    --					"cssmodules_ls",
-                    --					"dockerls",
-                    --					"docker_compose_language_service",
-                    --					"html",
-                    --					"jsonls",
-                    --					"ts_ls", -- Javascript
-                    --					"ltex",
-                    --					"texlab", -- LaTeX
-                    "lua_ls",   -- Lua
-                    "marksman", -- Markdown
-                    "phpactor", -- PHP
-                    -- "pylsp",
-                    --					"ruby_lsp",
-                    --					"sqlls",
-                    --					"lemminx", -- XML
-                    --					"hydra_lsp", -- YAML
-                    "bashls", -- Bash
-                },
-            })
-            mason_tool_installer.setup({
-                auto_update = false,
-                ensure_installed = {
-                    "clang-format", -- C/C++ formatter_path
-                    --	"cpplint", -- C/C++ Linter -- removed not very good
-                    --					"cmakelang",
-                    --					"cmakelint", -- CMake linter
-                    --					"bibtex-tidy", -- Bibtex
-                    --					"prettier", -- prettier formatter
-                    "stylua", -- lua formatter
-                    "shfmt",  -- shell formatter
-                    --					"isort", -- python formatter
-                    --					"black", -- python formatter
-                    --					"pylint", -- python linter
-                    --					"eslint_d", -- js linter
-                    --					"hadolint", -- docker linter
-                    "markdownlint-cli2", -- markdown formatter
-                    "markdown-toc",      -- mardown formatter
-                    "markdownlint",      -- markdown linter
-                    "phpcs",             -- PHP linter
-                    "php-cs-fixer",      -- PHP formatter
-                },
-                integrations = {
-                    ['mason-lspconfig'] = true,
-                    ['mason-null-ls'] = false,
-                    ['mason-nvim-dap'] = true,
-                },
-            })
-            mason_nvim_dap.setup({
-                automatic_installation = true,
-                ensure_installed = {
-                    "codelldb", -- C /CPP / Rust
-                    "php"       -- php-debug-adapter -- PHP
-                },
-            })
+            mason.setup(opts)
         end,
     },
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        opts = {
+            ensure_installed = {
+                -- LSP Config Section
+                "clangd",             -- C/C++
+                "lua_ls",             -- Lua
+                "marksman",           -- Markdown
+                "phpactor",           -- PHP
+                "bashls",             -- Bash
+                -- Linters and Formatters
+                "clang-format",       -- C/C++ formatter_path
+                "stylua",             -- lua formatter
+                "shfmt",              -- shell formatter
+                "markdownlint-cli2",  -- markdown formatter
+                "markdown-toc",       -- mardown formatter
+                "markdownlint",       -- markdown linter
+                "phpcs",              -- PHP linter
+                "php-cs-fixer",       -- PHP formatter
+                -- DAP
+                "bash-debug-adapter", -- Bash
+                "codelldb",           -- C /CPP / Rust
+                "php-debug-adapter",  -- PHP
+
+            }
+        }
+    }
 }
