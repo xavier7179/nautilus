f77c646b66e1f84a97156148fcb67c689d6eeed6
Ale <notinmyhead@gmail.com>
2026-06-11 13:02:22 +0200

    completions: update lazydev library config - remove LazyVim, fix lazy.nvim words
    - Remove LazyVim library entry (not needed)
    - Fix lazy.nvim words from 'LazyVim' to 'lazy'

--- lua/nautilus/plugins/completions.lua
+++ lua/nautilus/plugins/completions.lua
@@ -6,9 +6,8 @@ return {
 		opts = {
 			library = {
 				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-				{ path = "LazyVim", words = { "LazyVim" } },
 				{ path = "snacks.nvim", words = { "Snacks" } },
-				{ path = "lazy.nvim", words = { "LazyVim" } },
+				{ path = "lazy.nvim", words = { "lazy" } },
 			},
 		},
 	},
